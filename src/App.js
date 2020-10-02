6fe7aecfa8e73c4d2ae371f89f34307c98fc26ba
Finbar <finbarccowan@gmail.com>
2020-10-02 12:18:34 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ class App extends Component {
 
         try {
             this.setState({
-                // eslint-disable-next-line
                 result: (eval(checkResult) || "" ) + ""
             })
         } catch (e) {
